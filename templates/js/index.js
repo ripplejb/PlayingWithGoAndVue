22d7b624bd86d76f342e61c4ebbdb427b8be1453
Ripal Barot <ripplejb@hotmail.com>
2018-08-04 02:05:27 -0500

    Refactored Javascript/html code and removed unnecessary code.

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,23 +1,3 @@
-Vue.component('datacell', {
-    delimiters: ['${', '}'],
-    props: {
-        widthPercent: String,
-        value: String,
-        cssClassName: String
-    },
-    template: '<td :width="widthPercent" :class="cssClassName"> ${value}</td>'
-});
-
-Vue.component('headercell', {
-    delimiters: ['${', '}'],
-    props: {
-        widthPercent: String,
-        value: String,
-        cssClassName: String
-    },
-    template: '<th :width="widthPercent" :class="cssClassName"> ${value}</th>'
-});
-
 var app = new Vue({
     el: '#app',
     delimiters: ['${', '}'],
@@ -27,29 +7,12 @@ var app = new Vue({
         tableHeaderInfo: {}
     },
     methods: {
-        createDataCellDef(results, columnWidths) {
-            var datainfo = [];
-            var i = 0;
-            for(var prop in results) {
-               datainfo.push({value: results[prop], widthPercent: columnWidths[i++], cssClassName: 'mydatacell'})
-            }
-            return datainfo;
-        },
-        createHeaderDef(headers, columnWidths) {
-            var headerInfo = [];
-
-            for(var i=0; i < headers.length; i++) {
-                headerInfo.push({value:headers[i], widthPercent: columnWidths[i], cssClassName:'myheadercell'})
-            }
-
-            return headerInfo;
-        },
         sendData() {
             this.$http.get('/search?searchInput=' + this.searchInput).then((response) => {
                 this.searchOutput = JSON.parse(response.bodyText);
-                this.tableHeaderInfo = this.createHeaderDef(this.searchOutput.Headers, this.searchOutput.ColumnWidths)
+                this.tableHeaderInfo = createHeaderDef(this.searchOutput.Headers, this.searchOutput.ColumnWidths)
                 for(let ind in this.searchOutput.Results) {
-                    this.searchOutput.Results[ind]['celldef'] = this.createDataCellDef(this.searchOutput.Results[ind], this.searchOutput.ColumnWidths);
+                    this.searchOutput.Results[ind]['celldef'] = createDataCellDef(this.searchOutput.Results[ind], this.searchOutput.ColumnWidths);
                 }
             });
         },
