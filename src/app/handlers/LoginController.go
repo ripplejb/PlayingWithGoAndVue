39467e69c84d439989ae9066c8741f3ae8512597
Ripal Barot <ripplejb@hotmail.com>
2018-08-21 23:19:42 -0500

    Login controller partially implemented. Played little more with colors in css.

--- src/app/handlers/LoginController.go
+++ src/app/handlers/LoginController.go
@@ -9,6 +9,17 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
 	responseTemplate := template.Must(template.ParseFiles("templates/login/login.html"))
 
+	r.ParseForm()
+
+	if r.Form.Get("user-id") == "abc@abc" && r.Form.Get("password") == "test" {
+		responseTemplate := template.Must(template.ParseFiles("templates/main/index.html"))
+
+		if err := responseTemplate.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
 	data := struct{ Error string }{"Invalid Login..."}
 
 	if err := responseTemplate.Execute(w, data); err != nil {
--- src/app/handlers/RootController.go
+++ src/app/handlers/RootController.go
@@ -7,12 +7,6 @@ import (
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 
-	//responseTemplate := template.Must(template.ParseFiles("templates/main/index.html"))
-	//
-	//if err := responseTemplate.ExecuteTemplate(w, "index.html", nil); err != nil {
-	//	http.Error(w, err.Error(), http.StatusInternalServerError)
-	//}
-
 	responseTemplate := template.Must(template.ParseFiles("templates/login/login.html"))
 
 	if err := responseTemplate.Execute(w, nil); err != nil {
