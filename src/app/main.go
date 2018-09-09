269e0a0d83cb7ce05f8632f5959b8ce1aa2e0b5b
Ripal Barot <ripplejb@hotmail.com>
2018-09-09 16:56:15 -0500

    Updating for the appengine.

--- src/app/main.go
+++ src/app/main.go
@@ -16,19 +16,5 @@ func main() {
 	ng.UseHandler(routing.GetRouter())
 	http.Handle("/", ng)
 
-	//fs := http.FileServer(http.Dir("static"))
-	//http.Handle("/static/", http.StripPrefix("/static/", fs))
-	//http.HandleFunc("/", handlers.RootHandler)
-	//http.HandleFunc("/Earning", handlers.GetEarning)
-	//http.HandleFunc("/Dividend", handlers.GetDividend)
-	//http.HandleFunc("/dd44c44a-590f-4fba-bae0-aced5131fcd7/Import", handlers.Import)
-	//http.HandleFunc("/login", handlers.LoginHandler)
-	//http.HandleFunc("/login-ui", handlers.LoginUIHandler)
-	//http.HandleFunc("/register-ui", handlers.RegisterUIHandler)
-	//http.HandleFunc("/register", handlers.RegisterHandler)
-	//http.HandleFunc("/check-username", handlers.UsernameAvailability)
-	//http.HandleFunc("/favicon.ico", func(writer http.ResponseWriter, request *http.Request) {
-	//	http.ServeFile(writer, request, "./static/favicon.ico")
-	//})
 	appengine.Main()
 }
